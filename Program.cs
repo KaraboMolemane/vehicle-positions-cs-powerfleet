f6c5fc570874b74f80d9aef9988a6b2035b35683
KaraboMo <karabo.molemane@adaptit.com>
2024-11-09 15:35:02 +0200

    initial commit: set up vehicle_positions_cs_powerfleet project with basic structure and Hello World output

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace vehicle_positions_cs_powerfleet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
